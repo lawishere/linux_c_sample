6acadc7a212e81d2c73ed26ed7b77bdc27dc7d35
Frank Law <frank@juanvision.com>
2015-07-06 20:39:12 +0800

    Remove "struct dirent" Using.

--- linux_dir.c
+++ linux_dir.c
@@ -21,9 +21,9 @@
 void foreach_dir(char *path, int depth)
 {
 	DIR *Dir;
-	struct dirent *Entry;
 	struct stat statbuf;
 	char whole_path[1024];
+	struct dirent *Entry = NULL;
 
 	// Init Root Path
 	snprintf(whole_path, sizeof(whole_path), "%s", path);
@@ -37,29 +37,53 @@ void foreach_dir(char *path, int depth)
 
 	while (NULL != (Entry = readdir(Dir)))
 	{
-
-		lstat(Entry->d_name, &statbuf);
-		if (S_ISDIR(statbuf.st_mode))
+		if (0 == strcmp(".", Entry->d_name) || 0 == strcmp("..", Entry->d_name))
 		{
-			// Skip Local and Upper Directory Description
-			if (0 == strcmp(".", Entry->d_name)
-					|| 0 == strcmp("..", Entry->d_name))
-			{
-				continue;
-			}
-
-			printf("%*s%s\r\n", depth, " ", Entry->d_name);
-			if (0 == depth || depth > 1)
-			{
-				snprintf(whole_path + strlen(whole_path),
-						sizeof(whole_path) - strlen(whole_path), "/%s",
-						Entry->d_name);
-				foreach_dir(whole_path, depth > 1 ? depth - 1 : depth);
-			}
+			continue;
 		}
 		else
 		{
-			printf("%s/%s\r\n", whole_path, Entry->d_name);
+			char sub_path[1024];
+			snprintf(sub_path, sizeof(sub_path), "%s/%s", whole_path,
+					Entry->d_name);
+
+			if (Entry->d_type & DT_DIR)
+			{
+				printf("Folder: \"%s\"\r\n", sub_path);
+
+				if (0 == depth || depth > 1)
+				{
+					foreach_dir(sub_path, depth > 1 ? depth - 1 : depth);
+				}
+			}
+			else if (Entry->d_type & DT_BLK)
+			{
+				printf("Block Device: \"%s\"\r\n", sub_path);
+			}
+			else if (Entry->d_type & DT_CHR)
+			{
+				printf("Character Device: \"%s\"\r\n", sub_path);
+			}
+			else if (Entry->d_type & DT_FIFO)
+			{
+				printf("Named PIPE: \"%s\"\r\n", sub_path);
+			}
+			else if (Entry->d_type & DT_LNK)
+			{
+				printf("Symbolic Link: \"%s\"\r\n", sub_path);
+			}
+			else if (Entry->d_type & DT_REG)
+			{
+				printf("Regular File: \"%s\"\r\n", sub_path);
+			}
+			else if (Entry->d_type & DT_SOCK)
+			{
+				printf("UNIX Domain Socket: \"%s\"\r\n", sub_path);
+			}
+			else if (Entry->d_type & DT_UNKNOWN)
+			{
+				printf("Unknow File: \"%s\"\r\n", sub_path);
+			}
 		}
 	}
 
@@ -69,7 +93,7 @@ void foreach_dir(char *path, int depth)
 int main()
 {
 	printf("Directory scan of /home:\n");
-	foreach_dir("/home", 2);
+	foreach_dir("/dev", 2);
 	printf("done.\n");
 	return 0;
 
